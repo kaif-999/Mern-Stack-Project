e7ab03b7d2eda7915c2c458f4d38ad5c15d3a4b8
kaif-999 <idrisikaif8427@gmail>
2024-10-22 19:01:06 +0530

    adding a project

---
+++ server/server.js
@@ -0,0 +1,30 @@
+    const express = require('express')
+    const mongoose = require('mongoose');
+    const cookieParser = require('cookie-parser');
+    const cors = require ('cors');
+    // create a database connection u can also 
+    // create a seprate file for this and then import / use that file here
+    
+    mongoose.connect('mongodb+srv://kaif:kaif@mongodb@cluster0.fnygw.mongodb.net/')
+    .then(() => comnsole.log ("MongoDB Connected"))
+    .catch((error) => console.log(error));
+
+    const app = express();
+    const port = process.env.PORT || 500;
+
+    app.use (
+cors({
+origin : 'http//localhost:5173/',
+methods : ['GET', 'POST', 'DELETE', 'PUT'],
+AllowHeaders : [
+    "Content-Type",
+    'Authorization',
+    'Cache-Control',
+    'Expires',
+    'Pragma'], credentials:true    
+
+})
+);
+
+app.use (cookieParser());
+app.use (express.json());
