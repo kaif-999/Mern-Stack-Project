fe4ea9bc3540d97410f64b47040bb698c1488e32
kaif-999 <idrisikaif8427@gmail>
2024-10-23 18:39:47 +0530

    adding authSlice

---
+++ e-commerce/src/store/auth-slice/index.js
@@ -0,0 +1,15 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+
+
+const initialState = {
+
+    isAuthanticated : 
+}
+
+
+const authSlice = createSlice ({
+    name : 'auth',
+    initialState    
+})
