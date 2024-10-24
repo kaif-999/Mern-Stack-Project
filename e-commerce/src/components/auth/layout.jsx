5d46f78b1a1c190f1c22899035b1975736a601b8
kaif-999 <idrisikaif8427@gmail>
2024-10-24 17:03:32 +0530

    adding app layout login and register

--- e-commerce/src/components/auth/layout.jsx
+++ e-commerce/src/components/auth/layout.jsx
@@ -2,10 +2,20 @@ function AuthLayout (){
     return (
 <div className = "flex min-h-screen w-full">
 <div className = "hidden lg:flex item-center justify-center bg-black w1/2 px-12">
+<div className = "max-w-md space-y-6 text-center text-primary-foreground">
+<h1 className="text-4xl font-extrabold tracking-tight">
+Welcome to E-commerce Shoping
+</h1>
+</div>
+</div>
+
+<div className="flex flex-1 items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
+    <outlet />
+</div>
 
 
 </div>
-</div>
+
     );
 }
  export default AuthLayout;
